89f4db2ce27374d6e6309472aaf3df9807f3de1f
Victor <asaptorvi@protonmail.com>
2023-01-20 02:12:08 -0300

    wip

---
+++ learning-k8s/main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "minikube"
+}
+
+resource "kubernetes_namespace" "productionex" {
+  metadata {
+    name = "production-teste"
+  }
+}
+
+resource "kubernetes_deployment" "productionex" {
+  metadata {
+    name      = "nginx"
+    namespace = kubernetes_namespace.productionex.metadata.0.name
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "MyTestApp"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "MyTestApp"
+        }
+      }
+      spec {
+        container {
+          image = "nginx"
+          name  = "nginx-container"
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+resource "kubernetes_service" "productionex" {
+  metadata {
+    name      = "nginx"
+    namespace = kubernetes_namespace.productionex.metadata.0.name
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment.productionex.spec.0.template.0.metadata.0.labels.app
+    }
+    type = "NodePort"
+    port {
+      node_port   = 30201
+      port        = 80
+      target_port = 80
+    }
+  }
+}
